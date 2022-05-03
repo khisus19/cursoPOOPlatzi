37002716a232874cd0919602b2b74e517ce5bf90
Jesús Álvarez <khisus19@gmail.com>
2022-05-03 12:50:45 -0500

    Creando objetos a partir de clases en JAVA y Python

--- Java/main.java
+++ Java/main.java
@@ -1,5 +1,16 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("Hola Mundo!");
+        Car car = new Car();
+        car.license = "AMQ123";
+        car.driver = "Andres Herrera";
+        car.passenger = 4;
+        car.printDataCar();
+
+        Car car2 = new Car();
+        car2.license = "QMFN324";
+        car2.driver = "Pedro Mendez";
+        car2.passenger = 3;
+        car2.printDataCar();
     }
 }
--- Python/main.py
+++ Python/main.py
@@ -1,2 +1,13 @@
+from car import Car
+
 if __name__ == "__main__":
     print("Hola Mundo")
+    car = Car()
+    car.license = "AMG522"
+    car.driver = "Andres Herrera"
+    print(vars(car))
+
+    car2 = Car()
+    car2.license = "QMFN324"
+    car2.driver = "Pedro Mendez"
+    print(vars(car2))
