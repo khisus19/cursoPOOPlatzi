684742720d16a98ba0b86f0cea58ad7fec258b97
Jesús Álvarez <khisus19@gmail.com>
2022-05-04 08:45:15 -0500

    Aplicando la herencia de clases en python

--- Python/main.py
+++ Python/main.py
@@ -1,13 +1,9 @@
 from car import Car
+from account import Account
 
 if __name__ == "__main__":
     print("Hola Mundo")
-    car = Car()
-    car.license = "AMG522"
-    car.driver = "Andres Herrera"
-    print(vars(car))
 
-    car2 = Car()
-    car2.license = "QMFN324"
-    car2.driver = "Pedro Mendez"
-    print(vars(car2))
+    car = Car("AMG522", Account("Andres Herrera", "AHE4832"))
+    print(vars(car))
+    print(vars(car.driver))
