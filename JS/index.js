3cc641c99ff11b1afdeba4b6b2f415b70e25e8a1
Jesús Álvarez <khisus19@gmail.com>
2022-05-04 11:49:21 -0500

    Agrego las herencias de los tipos de carros en JS

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,7 @@
 var car = new Car("HAJD93", new Account("Andres Herrera", "AHE920"));
 car.passenger = 4;
 car.printDataCar();
+
+var uberX = new Car("AMG395", new Account("Andrea Ferran", "ANF942"), "Chevrolet", "Spark");
+uberX.passenger = 4;
+uberX.printDataCar();
