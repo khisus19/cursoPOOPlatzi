fededd7cd322e08546bf25c1ab76f1d64557d1f3
Jesús Álvarez <khisus19@gmail.com>
2022-05-08 11:49:10 -0500

    Agregando Polimorfismo a las clases UberX UberVan

--- Java/Main.java
+++ Java/Main.java
@@ -2,11 +2,11 @@ class Main {
     public static void main(String[] args) {
         System.out.println("Hola Mundo!");
         UberX uberX = new UberX("AMQ123", new Account("Andres Herrera", "HRNE532"), "Chevrolet", "Sonic");
-        uberX.setPassenger(3);
+        uberX.setPassenger(4);
         uberX.printDataCar();
 
-        /* Car car2 = new Car("QMFN324", new Account("Pedro Mendez", "PME395"));
-        car2.passenger = 3;
-        car2.printDataCar(); */
+        UberVan uberVan = new UberVan("FHG4383", new Account("Luis Paredes", "JFI392"));
+        uberVan.setPassenger(6);
+        uberVan.printDataCar();
     }
 }
--- Java/UberVan.java
+++ Java/UberVan.java
@@ -4,7 +4,12 @@ import java.util.Map;
 class UberVan extends Car {
     Map<String, Map<String,Integer>> typeCarAccepted;
     ArrayList<String> seatsMaterial;
+    private Integer passenger;
     
+    public UberVan(String license, Account driver){
+        super(license, driver);
+    }
+
     public UberVan(String license, Account driver, 
     Map<String, Map<String,Integer>> typeCarAccepted,
     ArrayList<String> seatsMaterial){
@@ -12,4 +17,15 @@ class UberVan extends Car {
         this.typeCarAccepted = typeCarAccepted;
         this.seatsMaterial = seatsMaterial;
     }
+    
+    @Override
+    public void setPassenger(Integer passenger) {
+        if (passenger == 6) {
+            this.passenger = passenger;
+        } else {
+            System.out.println("Necesitas asignar 6 pasajeros");
+        }
+    }
+    
+
 }
