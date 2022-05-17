eac7bb3a6307d1c9f6b0edda3d9f8c9eadabfcd6
Jesús Álvarez <khisus19@gmail.com>
2022-05-17 17:07:40 -0500

    Herencia en Js

--- basicoPoo/Encapsulamiento-ES6/main.mjs
+++ basicoPoo/Encapsulamiento-ES6/main.mjs
@@ -23,3 +23,77 @@ export class PlatziClass {
         videostop(this.videoID);
     }
 }
+
+class Course {
+    constructor({
+        name,
+        classes = [],
+        isFree = false,
+        lang = "spanish",
+    }) {
+        this.name = name;
+        this.classes = classes;
+    }
+};
+
+class Student {
+    constructor({
+        name,
+        email,
+        username,
+        twitter = undefined,
+        instagram = undefined,
+        facebook = undefined,
+        approvedCourses = [],
+        learningPaths = [],
+    }) {
+        this.name = name;
+        this.email = email;
+        this.username = username;
+        this.socialMedia = {
+            twitter,
+            instagram,
+            facebook,
+        };
+        this.approvedCourses = approvedCourses;
+        this.learningPaths = learningPaths;
+    }
+}
+
+class FreeStudent extends Student {
+    constructor(props) {
+        super(props);
+    }
+
+    approveCourse(newCourse) {
+        if (newCourse.isFree) {
+            this.approvedCourses.push(newCourse);
+        } else {
+            console.warn("Lo sentimos, "+ this.name + " solo puedes ver cursos gratis")
+        }
+    }
+}
+
+class BasicStudent extends Student {
+    constructor(props) {
+        super(props);
+    }
+
+    approveCourse(newCourse) {
+        if (newCourse.lang !== "english") {
+            this.approvedCourses.push(newCourse);
+        } else {
+            console.warn("Lo sentimos, " + this.name + ", solo puedes tomar cursos básicos");
+        }
+    }
+}
+
+class ExpertStudent extends Student {
+    constructor(props) {
+        super(props);
+    }
+
+    approveCourse(newCourse) {
+        this.approvedCourses.push(newCourse);
+    }
+}
