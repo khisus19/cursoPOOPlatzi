8c2c68b1aace1f99eba24f16525c9fde39c4a9f5
Jesús Álvarez <khisus19@gmail.com>
2022-05-24 18:26:11 -0500

    Funciones recursivas

--- intermedioPoo/clase7.js
+++ intermedioPoo/clase7.js
@@ -1,4 +1,4 @@
-const obj1 = {
+/* const obj1 = {
     a: "a",
     b: "b",
     c: {
@@ -10,4 +10,18 @@ const obj1 = {
 const stringifiedComplexObj = JSON.stringify(obj1);
 console.log(stringifiedComplexObj);
 const obj2 = JSON.parse(stringifiedComplexObj);
-console.log(obj2)
+console.log(obj2) */
+
+// FUNCION RECURSIVA
+
+const numbersArray = [2,1,52,623,64,2,326,646,3,23,13,4,1];
+
+function recursiva(numbersArray){
+    if (numbersArray.length != 0){
+        const firstNum = numbersArray[0];
+        console.log(firstNum);
+        numbersArray.shift();
+        return recursiva(numbersArray);
+    }
+}
+
