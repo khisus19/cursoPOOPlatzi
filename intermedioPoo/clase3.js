3f2ece750a2ca06906fe82b91527270c1f74b457
Jesús Álvarez <khisus19@gmail.com>
2022-05-23 19:04:19 -0500

    Inicio del Curso intermedio de Poo de platzi

---
+++ intermedioPoo/clase3.js
@@ -0,0 +1,12 @@
+const juan = {
+    name: "Juanito",
+    age: 18,
+    approvedCourses: ["Curso 1"],
+    addCourse(newCourse){
+        this.approvedCourses.push(newCourse);
+    }
+};
+
+console.log(Object.keys(juan));
+console.log(Object.getOwnPropertyNames(juan));
+console.log(Object.entries(juan));
