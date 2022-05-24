f91a486538328acbd39f547bc42ecb9a7eaeb2db
Jesús Álvarez <khisus19@gmail.com>
2022-05-23 21:09:42 -0500

    Clase 4 - Object.seal y Object.freeze

--- intermedioPoo/clase3.js
+++ intermedioPoo/clase3.js
@@ -7,6 +7,11 @@ const juan = {
     }
 };
 
-console.log(Object.keys(juan));
+/* console.log(Object.keys(juan));
 console.log(Object.getOwnPropertyNames(juan));
-console.log(Object.entries(juan));
+console.log(Object.entries(juan)); */
+
+// Object.seal(juan);
+Object.freeze(juan);
+
+console.log(Object.getOwnPropertyDescriptors(juan));
